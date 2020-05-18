5969661e71399b38c3b88c09ace026c669a8e082
christina <christina@datagif.fr>
2020-05-18 15:10:05 +0200

    home list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="intro">
+      <p>Une terre sans des animaux </p>
+    </div>
+    <main>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua dkhlskhhsg</p>
+      <router-link to="/list"><button>Decouvrir des animaux</button></router-link>
+    </main>
+    
+   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
+/*export default {
   name: 'Home',
   components: {
     HelloWorld
   }
-}
+}*/
 </script>
+<style scoped>
+body{
+ margin: 0;
+  width: 100vw;
+  height:100vh;
+  display: flex;
+  flex-direction:column;
+  align-items: center;
+}
+
+.intro{
+  background-color:#062F11;
+  width: 100vw;
+  height:100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+}
+
+.home .intro p{
+  color: #fff;
+  font-family: sans-serif;
+  font-size: 6vw;
+}
+
+.home main{
+  width:100vw;
+  max-width:1440px;
+  display: flex;
+  flex-direction:column;
+  align-items: center;
+  margin-bottom: 5.5vw;
+
+}
+
+.home main p{
+  width:90vw;
+  text-align: center;
+  font-size: 40px;
+}
+
+
+
+
+</style>
---
+++ src/views/List.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="list">
+    <div class="intro">
+      <p>Une terre sans des animaux<br><span>Une list des animau menace</span></p>
+    </div>
+    <main>
+      <div class="wrapper__button">
+        <button>Decouvrir des animaux</button>
+      </div>
+      <div class="list__headline">
+        <p>Au revoir</p>
+        <h2>Tous nos candidates</h2>
+      </div>
+    
+      <div class="wrapper__list">
+        <div class="wrapper__list__elmenet"></div>
+        <div class="wrapper__list__elmenet"></div>
+        <div class="wrapper__list__elmenet"></div>
+        <div class="wrapper__list__elmenet"></div>
+        <div class="wrapper__list__elmenet"></div>
+        <div class="wrapper__list__elmenet"></div>
+        <div class="wrapper__list__elmenet"></div>
+      </div>
+      
+    </main>
+      
+  </div>
+</template>
+
+<style scoped>
+
+
+.list{
+   display: flex;
+  flex-direction:column;
+  align-items: center;
+  justify-content: center;
+}
+
+.list__headline{
+  text-align-last: left;
+  font-family: sans-serif;
+  line-height: 3vw;
+  margin-top: 2vw;
+  
+}
+.list__headline p,h2{
+  line-height: 3vw;
+  padding:0;
+  margin:0;
+  font-size: 3vw;
+}
+
+
+main{
+  width:100vw;
+  max-width:1440px;
+  margin-bottom: 5.5vw;
+  padding:2vw;
+  
+  margin-top: -40vh;
+  background-color: #fff;
+  width:90vw;
+  max-width:1200px;
+}
+
+.wrapper__button{
+  width:100%;
+  display: flex;
+  justify-content: flex-end;
+  
+}
+
+.wrapper__list{
+  margin-top: 20px;
+  display: flex;
+  justify-content:space-between;
+   flex-wrap:wrap;
+  
+}
+
+.wrapper__list__elmenet{
+  width:31%;
+  height:200px;
+  background-color:green;
+  margin-bottom: 20px;
+}
+
+@media only screen and (max-width: 600px) {
+  .wrapper__list__elmenet{
+    width:100%
+  }
+}
+
+.intro{
+  background-color:#062F11;
+  width: 100vw;
+  height:100vh;
+  
+}
+.intro p{
+  color: #fff;
+  font-family: sans-serif;
+  font-size: 6vw;
+  line-height: 3.5vw;
+  text-align: center;
+  margin-top: 30vh;
+}
+.intro span{
+  font-size: 3vw;
+}
+
+button{
+  background-color: #062F11;
+  color:#fff;
+  padding:20px;
+  border-radius:50px;
+  font-size: 20px;
+}
+
+</style>
