0806cf0ca3d45f37cce592afb853fe3efdfe5890
Tan <tanamanda13@gmail.com>
2020-05-19 12:32:55 +0200

    offline ok but no images

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,7 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 // permet d'acceder au precacheManifest
 
+// eslint-disable-next-line no-undef 
 workbox.setConfig({
   debug: true
 })
@@ -12,9 +13,12 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 // eslint-disable-next-line no-undef 
 workbox.routing.registerRoute(
   // new RegExp(`http://(.*)`),
-  new RegExp(`http://localhost:8887/list`),
-  // new RegExp(`http://localhost:8080/species/(.*)`),
+
+  // new RegExp(`http://www.bloowatch.org/(.*)`),
+  new RegExp(`/list`),
+  // new RegExp(`http://localhost:8887/species/(.*)`),
   // stratégie de mise en cache. Exemple avec CacheFirst
+  // eslint-disable-next-line no-undef 
   new workbox.strategies.CacheFirst({
     // Donner un nom au chache, c'est pour debug
     cacheName: 'species',
@@ -26,6 +30,63 @@ workbox.routing.registerRoute(
     plugins: [
       // Expiration de plugins. Gérer la durée de vie du chache d'expiration des page caché
       // Ici, on prépfère gérer plutot le nom de req max
+      // eslint-disable-next-line no-undef 
+      new workbox.expiration.Plugin({
+        // 30 requete max (après ca supprime)
+        maxEntries: 30
+      })
+    ]
+  })
+)
+// eslint-disable-next-line no-undef 
+workbox.routing.registerRoute(
+  // new RegExp(`http://(.*)`),
+
+  // new RegExp(`http://www.bloowatch.org/(.*)`),
+  // new RegExp(`http://localhost:8887/list`),
+  new RegExp(`/species/(.*)`),
+  // stratégie de mise en cache. Exemple avec CacheFirst
+  // eslint-disable-next-line no-undef 
+  new workbox.strategies.CacheFirst({
+    // Donner un nom au chache, c'est pour debug
+    cacheName: 'species',
+    // MISE EN CACHE SEUEMENT DES METHOD GET
+    method: "GET",
+    // seulement des statut entre 0 et 200
+    cacheableResponse: { statuses: [0, 200]},
+    // ajout de plugins
+    plugins: [
+      // Expiration de plugins. Gérer la durée de vie du chache d'expiration des page caché
+      // Ici, on prépfère gérer plutot le nom de req max
+      // eslint-disable-next-line no-undef 
+      new workbox.expiration.Plugin({
+        // 30 requete max (après ca supprime)
+        maxEntries: 30
+      })
+    ]
+  })
+)
+// eslint-disable-next-line no-undef 
+workbox.routing.registerRoute(
+  // new RegExp(`http://(.*)`),
+
+  new RegExp(`http://www.bloowatch.org/(.*)`),
+  // new RegExp(`http://localhost:8887/list`),
+  // new RegExp(`http://localhost:8887/species/(.*)`),
+  // stratégie de mise en cache. Exemple avec CacheFirst
+  // eslint-disable-next-line no-undef 
+  new workbox.strategies.CacheFirst({
+    // Donner un nom au chache, c'est pour debug
+    cacheName: 'species',
+    // MISE EN CACHE SEUEMENT DES METHOD GET
+    method: "GET",
+    // seulement des statut entre 0 et 200
+    cacheableResponse: { statuses: [0, 200]},
+    // ajout de plugins
+    plugins: [
+      // Expiration de plugins. Gérer la durée de vie du chache d'expiration des page caché
+      // Ici, on prépfère gérer plutot le nom de req max
+      // eslint-disable-next-line no-undef 
       new workbox.expiration.Plugin({
         // 30 requete max (après ca supprime)
         maxEntries: 30
